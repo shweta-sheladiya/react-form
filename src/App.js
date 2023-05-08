d1b2e3689399c366edbcc6b2cc19a43b9f9850be
shweta <shwetasheladiya921@gmail.com>
2023-05-08 15:40:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Form from './Components/form'
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h3>React Local Storage Tutorial</h3>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
